617ca992af389b251f5ef3a9f498bc328fb7e278
edb-c <edb.cofresi@gmail.com>
2020-01-31 10:16:36 -0500

    add

--- AlgoCasts/exercises/stack/index.js
+++ AlgoCasts/exercises/stack/index.js
@@ -3,13 +3,46 @@
 // should be a class with methods 'push', 'pop', and
 // 'peek'.  Adding an element to the stack should
 // store it until it is removed.
-// --- Examples
-//   const s = new Stack();
-//   s.push(1);
-//   s.push(2);
-//   s.pop(); // returns 2
-//   s.pop(); // returns 1
 
-class Stack {}
+
+class Stack {
+
+    constructor() {
+      this.data = [];
+    }
+
+    push(record) {
+      this.data.push(record);
+        
+    }
+
+    pop() {
+      return this.data.pop();
+    }
+
+    peek() {
+      return this.data[this.data.length - 1];
+    }
+
+
+
+
+
+
+
+
+
+
+}//end Stack
+
 
 module.exports = Stack;
+
+
+const s = new Stack();
+   s.push(1);
+   s.push(2);
+const t = s.pop(); // returns 2
+   s.pop(); // returns 1
+
+   console.log(t)
